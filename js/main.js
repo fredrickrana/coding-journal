0178e3765729f8b2a0a5cec5ed76d43bd51e606f
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-19 09:03:25 +0000

    added a sibling div[data-view] to HTML. created a function that returns a DOM tree. altered HTML and CSS to include an entries page as well as a new button. created dummy journal entry within the ul element.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $image = document.querySelector('img');
 var $photourl = document.querySelector('input[name="photourl"]');
 var $form = document.querySelector('form');
+// var $ul = document.querySelector('ul');
 
 function updateImageSource(event) {
   $image.setAttribute('src', $photourl.value);
@@ -29,3 +30,34 @@ function submitEntries(event) {
   $form.reset();
 }
 $form.addEventListener('submit', submitEntries);
+
+// function uploadNewEntry(event) {
+//   $ul.appendChild(entry);
+// }
+// window.addEventListener('DOMContentLoaded', uploadNewEntry);
+
+// function newEntry(entry) {
+//   var liElement = document.createElement('li');
+//   var divOne = document.createElement('div');
+//   divOne.setAttribute('class', 'row');
+//   liElement.appendChild(divOne);
+//   var divTwo = document.createElement('div');
+//   divTwo.setAttribute('class', 'column-half');
+//   divOne.appendChild(divTwo);
+//   var imgElement = document.createElement('img');
+//   imgElement.setAttribute('src', $photourl);
+//   imgElement.setAttribute('class', 'image-size');
+//   divTwo.appendChild(imgElement);
+//   var divThree = document.createElement('div');
+//   divThree.setAttribute('class', 'column-half');
+//   divOne.appendChild(divThree);
+//   var headerTwo = document.createElement('h2');
+//   headerTwo.setAttribute('class', 'journal-title');
+//   headerTwo.textContent = entry.title;
+//   divThree.appendChild(headerTwo);
+//   var paragraphElement = document.createElement('p');
+//   paragraphElement.setAttribute('class', 'journal-notes');
+//   paragraphElement.textContent = entry.notes;
+//   divThree.appendChild(paragraphElement);
+//   return liElement;
+// }
