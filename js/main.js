6b509e20c39572e909ec02830b7b3cafe3a24d49
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-19 09:20:55 +0000

    used a loop to create a DOM tree for each journal entry in the data model and append it to the page when DOMContentLoaded event is fired.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var $image = document.querySelector('img');
 var $photourl = document.querySelector('input[name="photourl"]');
 var $form = document.querySelector('form');
-// var $ul = document.querySelector('ul');
+var $ul = document.querySelector('ul');
+var $noEntries = document.querySelector('.no-entries');
 
 function updateImageSource(event) {
   $image.setAttribute('src', $photourl.value);
@@ -31,33 +32,37 @@ function submitEntries(event) {
 }
 $form.addEventListener('submit', submitEntries);
 
-// function uploadNewEntry(event) {
-//   $ul.appendChild(entry);
-// }
-// window.addEventListener('DOMContentLoaded', uploadNewEntry);
+function uploadNewEntry(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var allEntries = newEntry(data.entries[i]);
+    $ul.appendChild(allEntries);
+    $noEntries.className = 'hidden';
+  }
+}
+window.addEventListener('DOMContentLoaded', uploadNewEntry);
 
-// function newEntry(entry) {
-//   var liElement = document.createElement('li');
-//   var divOne = document.createElement('div');
-//   divOne.setAttribute('class', 'row');
-//   liElement.appendChild(divOne);
-//   var divTwo = document.createElement('div');
-//   divTwo.setAttribute('class', 'column-half');
-//   divOne.appendChild(divTwo);
-//   var imgElement = document.createElement('img');
-//   imgElement.setAttribute('src', $photourl);
-//   imgElement.setAttribute('class', 'image-size');
-//   divTwo.appendChild(imgElement);
-//   var divThree = document.createElement('div');
-//   divThree.setAttribute('class', 'column-half');
-//   divOne.appendChild(divThree);
-//   var headerTwo = document.createElement('h2');
-//   headerTwo.setAttribute('class', 'journal-title');
-//   headerTwo.textContent = entry.title;
-//   divThree.appendChild(headerTwo);
-//   var paragraphElement = document.createElement('p');
-//   paragraphElement.setAttribute('class', 'journal-notes');
-//   paragraphElement.textContent = entry.notes;
-//   divThree.appendChild(paragraphElement);
-//   return liElement;
-// }
+function newEntry(entry) {
+  var liElement = document.createElement('li');
+  var divOne = document.createElement('div');
+  divOne.setAttribute('class', 'row marginbottom-50');
+  liElement.appendChild(divOne);
+  var divTwo = document.createElement('div');
+  divTwo.setAttribute('class', 'column-half');
+  divOne.appendChild(divTwo);
+  var imgElement = document.createElement('img');
+  imgElement.setAttribute('src', entry.photourl);
+  imgElement.setAttribute('class', 'image-size');
+  divTwo.appendChild(imgElement);
+  var divThree = document.createElement('div');
+  divThree.setAttribute('class', 'column-half');
+  divOne.appendChild(divThree);
+  var headerTwo = document.createElement('h2');
+  headerTwo.setAttribute('class', 'journal-title');
+  headerTwo.textContent = entry.title;
+  divThree.appendChild(headerTwo);
+  var paragraphElement = document.createElement('p');
+  paragraphElement.setAttribute('class', 'journal-notes');
+  paragraphElement.textContent = entry.notes;
+  divThree.appendChild(paragraphElement);
+  return liElement;
+}
