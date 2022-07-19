82ac7a3a5bde441a54beeabaeb094e9f4a0cb2c4
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-19 22:57:30 +0000

    show entry form when an edit icon is clicked

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var $entriesNav = document.querySelector('.entries-header');
 var $newButton = document.querySelector('.button-new');
 var $entryForm = document.querySelector('.entry-form');
 var $entries = document.querySelector('.entries');
+// var $pencil = document.querySelectorAll('.fa-pencil');
+// var $li = document.querySelectorAll('li');
 
 function updateImageSource(event) {
   $image.setAttribute('src', $photourl.value);
@@ -27,7 +29,7 @@ function submitEntries(event) {
     title: $form.title.value,
     photourl: $form.photourl.value,
     notes: $form.notes.value,
-    entryId: data.nextEntry
+    entryId: data.nextEntryId
   };
   data.nextEntryId++;
   data.entries.unshift(entryValues);
@@ -56,6 +58,7 @@ function loadNewEntry(entry) {
 
 function newEntry(entry) {
   var liElement = document.createElement('li');
+  liElement.setAttribute('data-entry-id', entry.entryId);
   var divOne = document.createElement('div');
   divOne.setAttribute('class', 'row marginbottom-50');
   liElement.appendChild(divOne);
@@ -99,6 +102,8 @@ function newEntry(entry) {
   paragraphElement.textContent = entry.notes;
   divThree.appendChild(paragraphElement);
 
+  iElement.addEventListener('click', editAnEntry);
+
   return liElement;
 }
 
@@ -128,3 +133,40 @@ function stayOnPage(event) {
   viewSwap();
 }
 window.addEventListener('load', stayOnPage);
+
+function editAnEntry(event) {
+  data.view = 'entry-form';
+  viewSwap();
+  for (var i = 0; i < data.entries; i++) {
+    if (event.target.closest('li').getAttribute('data-entry-id').matches(data.entries[i].entryId)) {
+      data.editing = data.entries[i];
+    }
+  }
+}
+
+// __________________________________________________
+
+// parent element of all rendered entries is the li element
+
+// click on the pencil (which is the event.target)
+// go to the pencil's closest li element (ancestor)
+// get the attribute (data-entry-id) value of that li element
+
+// have the value match the entryId of an entry in the data model
+
+// ________________
+
+// look through each event.target
+// change editing to not null (?)
+// matches property
+
+// find the matching entry object in the data model and assign it to the data model's editing property if an edit icon was clicked
+// parent (ancestor) div of pencil is the li element
+// li element has a data-entry-id
+// use closest method ('li')
+
+// if the attribute of the event.target (which is the pencil) of the li element (which is a data-entry-id)... is equal / matches ... entryId in the multiple data.entries
+
+// change editing property
+// then pull the values of that editing property
+// append it to the form...
